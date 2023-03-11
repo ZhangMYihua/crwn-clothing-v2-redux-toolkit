437708c3036dfdbc85d7e4b7065d270957e6ce10
gabypega1997 <gabypega1997@gmail.com>
2023-03-11 11:24:10 +0100

    add a sample to test a func

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(`${process.env.STRIPE_SECRET_KEY}`);
 
 exports.handler = async (event) => {
     try {
--- src/components/payment-form/payment-from.component.jsx
+++ src/components/payment-form/payment-from.component.jsx
@@ -13,13 +13,22 @@ const PaymentForm = () => {
         if (!stripe || !elements) {
             return;
         }
-
-        
+        const response = await fetch(
+            "/.netlify/functions/create-payment-intent",
+            {
+                method: "post",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ amount: 10000 }),
+            }
+        ).then((res) => res.json());
+        console.log(response);
     };
 
     return (
         <PaymentFormContainer>
-            <FormContainer>
+            <FormContainer onSubmit={paymentHandler}>
                 <h2>Credit Card Payment: </h2>
                 <CardElement />
                 <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>

aa8cf4bd4039e082eef3c4ef39834579d6ee1222
gabypega1997 <gabypega1997@gmail.com>
2023-03-11 10:19:57 +0100

    create a element and a basic style for Card Element

--- src/components/payment-form/payment-from.component.jsx
+++ src/components/payment-form/payment-from.component.jsx
@@ -1,13 +1,32 @@
-import { CardElement } from "@stripe/react-stripe-js";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 
 import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 
+import { PaymentFormContainer, FormContainer } from "./payment-form.styles";
 const PaymentForm = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const paymentHandler = async (e) => {
+        e.preventDefault();
+
+        if (!stripe || !elements) {
+            return;
+        }
+
+        
+    };
+
     return (
-        <div>
-            <CardElement />
-            <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay now</Button>
-        </div>
+        <PaymentFormContainer>
+            <FormContainer>
+                <h2>Credit Card Payment: </h2>
+                <CardElement />
+                <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>
+                    Pay now
+                </Button>
+            </FormContainer>
+        </PaymentFormContainer>
     );
 };
 

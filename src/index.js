95e4971447e9a4f06c7ea673c169dc725fb3298a
gabypega1997 <gabypega1997@gmail.com>
2023-03-11 09:48:41 +0100

    setUp Stripe

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import { Elements } from "@stripe/react-stripe-js";
+import { stripePromise } from "./utils/stripe/stripe.utils";
 
 import App from "./App";
 import { store, persistor } from "./store/store";
@@ -14,9 +16,13 @@ const rootElement = document.getElementById("root");
 render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
+            <PersistGate loading={null} persistor={persistor}>
+                <BrowserRouter>
+                    <Elements stripe={stripePromise}>
+                        <App />
+                    </Elements>
+                </BrowserRouter>
+            </PersistGate>
         </Provider>
     </React.StrictMode>,
     rootElement
---
+++ src/utils/stripe/stripe.utils.js
@@ -0,0 +1,6 @@
+import { loadStripe } from "@stripe/stripe-js";
+
+export const stripePromise = loadStripe(
+    process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
+);
+

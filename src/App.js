54f18adf0c94fd543fcbac7401aeedf3e0234520
kaanygit <yasinkaan345@hotmail.com>
2023-06-30 18:38:19 +0300

    toolkit added
    The redux toolkit was integrated. The types and action files in the store file were deleted and placed in the reducer files.

--- src/App.js
+++ src/App.js
@@ -12,17 +12,20 @@ import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
-import { setCurrentUser } from './store/user/user.action';
+import { setCurrentUser } from './store/user/user.reducer';
 
 const App = () => {
   const dispatch = useDispatch();
+
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
       if (user) {
         createUserDocumentFromAuth(user);
       }
-      dispatch(setCurrentUser(user));
+      const pickedUser=user && (({accessToken,email})=>({accessToken,email}))(user);
+      console.log(setCurrentUser(pickedUser));
+      dispatch(setCurrentUser(pickedUser));
     });
 
     return unsubscribe;
--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -1,16 +1,20 @@
-import { USER_ACTION_TYPES } from './user.types';
+import { createSlice } from '@reduxjs/toolkit';
 
 const INITIAL_STATE = {
   currentUser: null,
 };
 
-export const userReducer = (state = INITIAL_STATE, action) => {
-  const { type, payload } = action;
-
-  switch (type) {
-    case USER_ACTION_TYPES.SET_CURRENT_USER:
-      return { ...state, currentUser: payload };
-    default:
-      return state;
+export const userSlice = createSlice({
+  name:'user',
+  initialState:INITIAL_STATE,
+  reducers:{
+    setCurrentUser(state,action){
+      state.currentUser=action.payload
+    }
   }
-};
+});
+
+export const {setCurrentUser}=userSlice.actions;
+
+export const userReducer=userSlice.reducer;
+

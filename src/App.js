316e112a32a23db030306d794f56f415723ce268
dastan1910 <dastan1910@gmail.com>
2023-08-24 10:33:48 +0600

    non serializerable values

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
-import { setCurrentUser } from './store/user/user.action';
+import { setCurrentUser } from './store/user/user.reducer';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -22,7 +22,11 @@ const App = () => {
       if (user) {
         createUserDocumentFromAuth(user);
       }
-      dispatch(setCurrentUser(user));
+      const pickedUser =
+          user && (({accessToken, email}) => ({accessToken, email}))(user)
+
+      console.log("1", setCurrentUser(pickedUser))
+      dispatch(setCurrentUser(pickedUser));
     });
 
     return unsubscribe;
--- src/store/store.js
+++ src/store/store.js
@@ -41,8 +41,8 @@ const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
 //
 export const store = configureStore({
       reducer: rootReducer,
-      middleware: middleWares,
-      // composedEnhancers
+      middleware: (getDefaultMiddleware) =>
+          getDefaultMiddleware().concat(middleWares),
 })
 
 

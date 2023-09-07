5c59a9401d17053aa6acb3eff1bb2f85530b79de
Jovan Banks <bmsHQ@icloud.com>
2023-09-07 14:23:36 -0500

    added styles to payment form file

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -2,16 +2,17 @@ import { useSelector } from 'react-redux';
 
 import {
   selectCartItems,
-  selectCartTotal,
+  selectCartTotal
 } from '../../store/cart/cart.selector';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import PaymentForm from '../../components/payment-form/payment-form-component';
 
 import {
   CheckoutContainer,
   CheckoutHeader,
   HeaderBlock,
-  Total,
+  Total
 } from './checkout.styles';
 
 const Checkout = () => {
@@ -41,6 +42,7 @@ const Checkout = () => {
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
       <Total>Total: ${cartTotal}</Total>
+      <PaymentForm />
     </CheckoutContainer>
   );
 };

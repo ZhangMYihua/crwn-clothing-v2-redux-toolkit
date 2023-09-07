8afe5d316b6bad30ca7ebc9976ffc20d9c75d0d8
louiebasalo <louiebasalo.public@gmail.com>
2023-09-07 02:18:12 +0000

    vid# 184. Using createSlice

--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -1,16 +1,23 @@
-import { USER_ACTION_TYPES } from './user.types';
+import { createSlice } from '@reduxjs/toolkit/dist';
 
 const INITIAL_STATE = {
   currentUser: null,
 };
 
-export const userReducer = (state = INITIAL_STATE, action) => {
-  const { type, payload } = action;
+export const userSlice = createSlice({
+  name: 'user', //the user in user/SET_CURRENT_USER in our user action type
+  initialState: INITIAL_STATE,
+  reducers: {
+    setCurrentUser(state, action){
+      state.currentUser = action.payload // this line here, although this looks like a mutation to the onject 'state', but under the hood it is not. Redux toolkit is leveraging a library called immer, it's just for the sake of readability
+                                         // as per immer, Of looks like a mutation, but instead of mutating this state object, let's create the same object, but with one differenc, this field currentUser
+                                         // now in RTK, when writing reducer using create slice, you are now writing mutation (so it seems) as code im the state itself and just rely on the RTK under the hood for the generation of the brand new State object.
+    }
+  } //an object with a function inside
+});
 
-  switch (type) {
-    case USER_ACTION_TYPES.SET_CURRENT_USER:
-      return { ...state, currentUser: payload };
-    default:
-      return state;
-  }
-};
+export const { setCurrentUser } = userSlice.actions;
+
+export const userReducer = userSlice.reducer;
+
+

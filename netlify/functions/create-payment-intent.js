c63bfdac26d48bdc8119239121e6813759e2cf5d
Jovan Banks <bmsHQ@icloud.com>
2023-09-07 21:37:00 -0500

    updated package.json

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,5 +1,5 @@
-require('dotenv').config();
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event) => {
   try {
@@ -7,20 +7,20 @@ exports.handler = async (event) => {
 
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
-      currency: 'usd',
-      payment_method_types: ['card']
+      currency: "usd",
+      payment_method_types: ["card"],
     });
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ paymentIntent })
+      body: JSON.stringify({ paymentIntent }),
     };
   } catch (error) {
     console.log({ error });
 
     return {
       status: 400,
-      body: JSON.stringify({ error })
+      body: JSON.stringify({ error }),
     };
   }
 };
